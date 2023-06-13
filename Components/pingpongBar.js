25dcc198cbd516cc3b0168caa1e0f70399bfee9a
Yash Baddi <yashbaddi29@gmail.com>
2023-06-13 10:08:20 +0530

    CSS Changes

--- Components/pingpongBar.js
+++ Components/pingpongBar.js
@@ -1,5 +1,21 @@
-import { createDOMElement } from "../utils";
+import { createDOMElement } from "../utils.js";
 
-const bar = createDOMElement("div", ["bar"], [], {});
+export function pingpongBar(position = "bottom") {
+  const bar = createDOMElement("div", ["bar", "bar-" + position], [], {});
+  if (position == "bottom") {
+    bar.tabIndex = 0;
+    bar.autofocus = true;
+  }
 
-const ball = createDOMElement("div", ["ball"], [], {});
+  bar.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft") {
+      console.log(event.target.style);
+
+      //   event.target.style.left =
+      //     parseInt(event.target.style.left || 0) + 1 + "rem";
+    }
+    if (event.key === "ArrowRight") {
+    }
+  });
+  return bar;
+}
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import { pingpongBar } from "./Components/pingpongBar.js";
 import { createDOMElement } from "./utils.js";
 
 const app = document.getElementById("app");
 
-const bar = createDOMElement("div", ["bar"], [], {});
+const bar = pingpongBar();
+// bar.focus();
 
 const ball = createDOMElement("div", ["ball"], [], {});
 
