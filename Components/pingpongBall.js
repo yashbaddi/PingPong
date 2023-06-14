23ac37e1a760b05ec3240a454bb4e71353586b66
Yash Baddi <yashbaddi29@gmail.com>
2023-06-14 13:20:55 +0530

    fixed the linearAnimate Function

--- Components/pingpongBall.js
+++ Components/pingpongBall.js
@@ -8,16 +8,10 @@ export function linearAnimate(div, interval = 20) {
   let topGrowthRate = 1;
   setInterval(() => {
     const computedStyle = getComputedStyle(div);
-    console.log(
-      computedStyle.left,
-      computedStyle.right,
-      computedStyle.top,
-      computedStyle.bottom
-    );
-    if (computedStyle.left === "1px") leftGrowthRate = 1;
-    if (computedStyle.right === "1px") leftGrowthRate = -1;
-    if (computedStyle.top === "1px") topGrowthRate = 1;
-    if (computedStyle.bottom === "1px") topGrowthRate = -1;
+    if (parseInt(computedStyle.left) <= 2) leftGrowthRate = 1;
+    if (parseInt(computedStyle.right) <= 2) leftGrowthRate = -1;
+    if (parseInt(computedStyle.top) <= 2) topGrowthRate = 1;
+    if (parseInt(computedStyle.bottom) <= 2) topGrowthRate = -1;
 
     div.style.left = parseInt(computedStyle.left) + leftGrowthRate + "px";
     div.style.top = parseInt(computedStyle.top) + topGrowthRate + "px";
