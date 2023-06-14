05c954940f445a543f5d6075688519290772f3de
Yash Baddi <yashbaddi29@gmail.com>
2023-06-14 18:04:40 +0530

    Added Incremental speed

--- Components/pingpongBall.js
+++ Components/pingpongBall.js
@@ -1,14 +1,22 @@
-export function linearAnimate(div, interval = 20) {
-  let leftGrowthRate = 1;
-  let topGrowthRate = 1;
+export function linearAnimate(
+  div,
+  interval = 20,
+  speed = 1,
+  speedIncrementRate = 0.001
+) {
+  let rightDirection = 1;
+  let bottomDirection = 1;
   setInterval(() => {
     const computedStyle = getComputedStyle(div);
-    if (parseInt(computedStyle.left) <= 2) leftGrowthRate = 1;
-    if (parseInt(computedStyle.right) <= 2) leftGrowthRate = -1;
-    if (parseInt(computedStyle.top) <= 2) topGrowthRate = 1;
-    if (parseInt(computedStyle.bottom) <= 2) topGrowthRate = -1;
+    if (parseInt(computedStyle.left) <= 2) rightDirection = 1;
+    if (parseInt(computedStyle.right) <= 2) rightDirection = -1;
+    if (parseInt(computedStyle.top) <= 2) bottomDirection = 1;
+    if (parseInt(computedStyle.bottom) <= 2) bottomDirection = -1;
+    speed = speed + speed * speedIncrementRate;
 
-    div.style.left = parseInt(computedStyle.left) + leftGrowthRate + "px";
-    div.style.top = parseInt(computedStyle.top) + topGrowthRate + "px";
+    div.style.left =
+      parseInt(computedStyle.left) + speed * rightDirection + "px";
+    div.style.top =
+      parseInt(computedStyle.top) + speed * bottomDirection + "px";
   }, interval);
 }
