af95b982075659b374852a36dfce5cd0d6056796
Yash Baddi <yashbaddi29@gmail.com>
2023-06-27 15:44:02 +0530

    added game over

--- Client/Services/isGameOver.js
+++ Client/Services/isGameOver.js
@@ -1,14 +1,19 @@
 import { game } from "../Store/gameStatus.js";
 
 export function isGameOver() {
+  console.log("Game over check");
   const ball = game.ball.pos;
   const paddleLeftEdge = game.paddle.mainPaddle;
   const ballWidth = 20;
   const paddleWidth = 190;
   const paddleRightEdge = paddleLeftEdge + paddleWidth;
   const ballLeftEdge = ball.x - ballWidth / 2;
-  const ballRightEdge = ball.x - ballWidth / 2;
-  if (ballRightEdge >= paddleLeftEdge) return false;
-  if (ballLeftEdge >= paddleRightEdge) return false;
+  const ballRightEdge = ball.x + ballWidth / 2;
+  if (ballRightEdge >= paddleLeftEdge && ballRightEdge <= paddleRightEdge) {
+    return false;
+  }
+  if (ballLeftEdge <= paddleRightEdge && ballLeftEdge >= paddleLeftEdge) {
+    return false;
+  }
   return true;
 }
