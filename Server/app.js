b8f6517516cbab1d2d6a1a2ac7e7e0668faf5c3c
Yash Baddi <yashbaddi29@gmail.com>
2023-06-30 10:44:48 +0530

    static server

--- Server/app.js
+++ Server/app.js
@@ -6,6 +6,11 @@ import wsRouter from "./Socket/socket.js";
 const app = express();
 expressWs(app);
 
+app.use(express.static("../Client"));
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
 app.use("/ws", wsRouter);
 
 app.listen(8080, () => {
